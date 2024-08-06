fafb775bf8cde249e829ebeb05f6675a06ef61d7
Scott Seto <scott.seto@mitechnicalsolutions.com>
2024-08-06 14:02:15 -0400

    Update materials

--- hype-studio/src/stories/structural-analysis/MaterialLibrary.tsx
+++ hype-studio/src/stories/structural-analysis/MaterialLibrary.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
 
 const materials = [
-  { name: 'Steel', youngsModulus: 200e9, poissonRatio: 0.3, yieldStrength: 250e6, fatigueStrength: 200e6, fatigueExponent: 10, thermalExpansionCoeff: 12e-6, density: 7850 },
-  { name: 'Aluminum', youngsModulus: 70e9, poissonRatio: 0.33, yieldStrength: 300e6, fatigueStrength: 120e6, fatigueExponent: 8, thermalExpansionCoeff: 23e-6, density: 2700 },
+  { 
+    name: 'Steel', 
+    youngsModulus: 200e9, 
+    poissonRatio: 0.3, 
+    yieldStrength: 250e6, 
+    fatigueStrength: 200e6, 
+    fatigueExponent: 10, 
+    thermalExpansionCoeff: 12e-6, 
+    density: 7850,
+    thermalConductivity: 45, // W/mK
+    specificHeat: 500, // J/kgK
+  },
+  { 
+    name: 'Aluminum', 
+    youngsModulus: 70e9, 
+    poissonRatio: 0.33, 
+    yieldStrength: 300e6, 
+    fatigueStrength: 120e6, 
+    fatigueExponent: 8, 
+    thermalExpansionCoeff: 23e-6, 
+    density: 2700,
+    thermalConductivity: 237, // W/mK
+    specificHeat: 897, // J/kgK
+  },
   // Add more materials as needed
 ];
 
