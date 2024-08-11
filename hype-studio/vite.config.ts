53496531ed2d02d17a05bec0f8fd74a617183283
Scott Seto <scott.seto@outlook.com>
2024-08-11 01:32:33 -0400

    Fix ts issue.

--- hype-studio/vite.config.ts
+++ hype-studio/vite.config.ts
@@ -1,56 +1,20 @@
-import { defineConfig, Plugin } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from "vite-plugin-svgr";
-import { vitePluginTrace } from './src/tracing';
-import { transformSync } from '@babel/core';
-import { babelPluginRemoveTracing } from './src/tracing';
-import viteTracePlugin from './src/tracing/server/viteTracePlugin';
-import path, { extname } from 'path';
-import babel from '@rollup/plugin-babel';
+import { defineConfig, PluginOption} from 'vite'
+import path from 'path';
+import babel, { RollupBabelInputPluginOptions } from '@rollup/plugin-babel';
 
-function createRemoveTracingPlugin(): Plugin {
-  return {
-    name: 'remove-tracing',
-    transform(code, id) {
-      if (id.endsWith('.js') || id.endsWith('.jsx') || id.endsWith('.ts') || id.endsWith('.tsx')) {
-        const result = transformSync(code, {
-          plugins: [babelPluginRemoveTracing],
-          filename: id,
-        });
-        return result?.code || code;
-      }
-    },
-  };
-}
+const options: RollupBabelInputPluginOptions = {
+  babelHelpers: 'bundled',
+  extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  presets: ["@babel/preset-react"],
+  plugins: [
+    ["@babel/plugin-proposal-decorators", { legacy: true }],
+    ["@babel/plugin-proposal-class-properties", { loose: true }]
+  ]
+};
 
 export default defineConfig(({ mode }) => ({
   plugins: [
-    react({
-      babel: {
-        plugins: [
-          ["@babel/plugin-proposal-decorators", { legacy: true }],
-          ["@babel/plugin-proposal-class-properties", { loose: true }]
-        ]
-      }
-    }),
-    babel({
-      babelHelpers: 'bundled',
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      presets: ["@babel/preset-react"],
-      plugins: [
-        ["@babel/plugin-proposal-decorators", { legacy: true }],
-        ["@babel/plugin-proposal-class-properties", { loose: true }]
-      ]
-    }),
-    svgr(),
-    ...(mode === 'development' 
-      ? [vitePluginTrace({ effectName: 'MyComponentEffect' }),
-        viteTracePlugin({
-          include: ['src/utils'],
-          exclude: ['node_modules', 'test']
-        })
-      ] 
-      : [createRemoveTracingPlugin()]),
+    babel(options) as PluginOption
   ],
   resolve: {
     alias: {
